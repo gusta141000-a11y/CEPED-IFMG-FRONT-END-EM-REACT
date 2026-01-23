de9869c76b5ae39d4171c4296a33da27dfd6a2a1
Gustavo Dos Reis Rodrigues <0116998@ifmg.edu.br>
2026-01-23 15:58:46 -0300

    adicionando aula 33

--- aula30/my-react-app/src/App.jsx
+++ aula30/my-react-app/src/App.jsx
@@ -2,9 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/aula33/Home.jsx'
+import Contact from './components/aula33/contact.jsx'
+import About from './components/aula33/About.jsx'
 
 
-function App(props) {
-    return
+function App() {
+    return(
+        <BrowserRouter>
+        {/* Navegação */}
+        <nav>
+            <Link to="/">Home</Link> |{" "}
+            <Link to="/about">About</Link> |{" "}
+            <Link to="/contact">Contact</Link>
+      </nav>
+        {/* Routes  */}
+        <Routes>
+            <Route path='/' element  = {<Home/>}/>
+            <Route path='/About' element  = {<About/>}/>
+            <Route path='/Contact' element  = {<Contact/>}/>
+        </Routes>
+
+        </BrowserRouter>
+    );
 }
 export default App
